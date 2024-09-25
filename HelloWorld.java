6d00b1b37fd9b46c5f76e235919e91db9a6e3120
Shyam Mashru <mashrushyam030303@gmail.com>
2024-09-26 00:38:40 +0530

    Commit

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld{
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Shyam Mashru");
+        System.out.println("Containerizing application using Docker Container. ");
+        System.out.println("Jenkins pipeline!");
+    }
+}
